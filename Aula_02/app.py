891d22cf94fce36de6bab4e52256f1c8d7d29f7b
CDFAlex <alexfermino1@gmail.com>
2025-08-14 12:00:22 -0300

    Aula 2: Continuação do estudo de variaveis

---
+++ Aula_02/app.py
@@ -0,0 +1,33 @@
+# Nome  = "João"
+# Idade = 20
+# Salario = 20000.10
+# Empregado = True
+
+# print("\n//-------------------//")
+# print("Nome: ", Nome)
+# print("Idade: ", Idade)     
+# print("Salário: ", Salario)
+# print("Empregado: ", Empregado)
+
+
+# lista_de_nomes = ["Ana", "Bia", "Carlos"]
+# print("\n//-------------------//")
+# print("Lista de nomes: ", lista_de_nomes)
+
+# pessoas = {"Nome": "Ana",
+#           "Idade": 25,
+#             "Salario": 3000.50}
+# print("\n//-------------------//")
+# print("Dicionário de pessoas: ", pessoas)  
+
+# nome = input("Digite seu nome: ")
+# print("seu nome é: ", nome)
+# idade = int(input("\nDigite sua idade: "))    
+# print("sua idade é: ", idade)
+
+# idade = int(input("Digite sua idade: "))
+# print("sua idade é: ", idade,"do tipo", type(idade))
+
+idade = int(input("Digite sua idade: "))
+print(f"Sua idade é: {idade} do tipo: {type(idade)}")
+   
